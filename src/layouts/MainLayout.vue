a4bd0f2ef780b56373bf72534828c82f56924d6a
avronex <70800084+avronex@users.noreply.github.com>
2020-09-09 10:35:55 +0200

    Add files via upload

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class='shadow-5'>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="drawer = !drawer"
+        />
+
+        <q-toolbar-title>
+          Erik Elze - Events
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+      <q-drawer
+        v-model="drawer"
+        show-if-above
+        :width="300"
+        :breakpoint="800"
+      >
+        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+          <q-list padding>
+            <q-item to='/' exact clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+              <q-item-section>
+                Material
+              </q-item-section>
+            </q-item>
+            <q-item to='/vermietung' exact clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="list" />
+              </q-item-section>
+              <q-item-section>
+                Vermietung
+              </q-item-section>
+            </q-item>
+          </q-list>
+          <div class="darkSwitch">
+            <q-toggle
+            v-model="darkMode"
+            @click='setDarkMode' />
+          </div>
+        </q-scroll-area>
+
+        <q-img class="absolute-top bg-primary shadow-5" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Erik Elze</div>
+            <div>24, Hannover</div>
+          </div>
+        </q-img>
+      </q-drawer>
+
+    <q-page-container>
+      <keep-alive>
+        <router-view />
+      </keep-alive>
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  data () {
+    return {
+      darkMode: false,
+      drawer: false
+    }
+  },
+  methods: {
+    setDarkMode () {
+      this.$q.dark.set(this.darkMode)
+    }
+  }
+}
+</script>
---
+++ src/pages/Vermietung.vue
@@ -0,0 +1,118 @@
+<template>
+  <q-page class='bg-grey-3 column'>
+    <div class="bg-white q-pa-sm">
+      <q-input
+      @keyup.enter="addObject"
+      v-model="newObject"
+      placeholder="Material, Preis"
+      dense>
+        <template v-slot:append>
+          <q-btn
+          @click='addObject'
+          round
+          dense
+          flat
+          icon="add" />
+        </template>
+      </q-input>
+    </div>
+    <q-list
+      class='bg-white'
+      separator
+      bordered>
+      <q-item
+        v-for='(object, index) in objects'
+        :key='object.title'
+        @click='object.packed = !object.packed'
+        :class="{ 'packed bg-grey-3' : object.packed }"
+        clickable
+        v-ripple>
+        <q-item-section avatar>
+          <q-checkbox
+          v-model="object.packed"
+          class="no-pointer-events"
+          color="secondary" />
+        </q-item-section>
+        <q-item-section>
+          <q-item-label>
+            {{ object.title }} - {{ object.price }}€
+          </q-item-label>
+        </q-item-section>
+        <q-item-section
+        v-if="object.packed"
+        side>
+          <q-btn
+          @click.stop='deleteObject(index)'
+          flat
+          round
+          color="primary"
+          icon="delete" />
+        </q-item-section>
+      </q-item>
+    </q-list>
+    <div v-if="!objects.length" class="no-objects text-center absolute-center">
+      <q-icon
+      name="check"
+      size="100px"
+      color="primary"
+      />
+      <div class="text-h6 text-primary text-center">
+        Kein Vermietmaterial
+      </div>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      newObject: '',
+      objects: [
+        // {
+        //   title: 'PA',
+        //   packed: false,
+        //   price: 200
+        // },
+        // {
+        //   title: 'LED',
+        //   packed: true,
+        //   price: 20
+        // },
+        // {
+        //   title: 'Nebel',
+        //   packed: false,
+        //   price: 30
+        // }
+      ]
+    }
+  },
+  methods: {
+    deleteObject (index) {
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'Material entfernen?',
+        cancel: true,
+        persistent: true
+      }).onOk(() => {
+        this.objects.splice(index, 1)
+      })
+    },
+    addObject () {
+      var newPrice = this.newObject.replace(/\D/g, '')
+      var newObjectName = JSON.stringify(this.newObject).replace(/[^A-Za-z-_]/g, '')
+      this.objects.push({
+        title: newObjectName,
+        packed: false,
+        price: newPrice
+      })
+      this.newObject = ''
+    }
+  }
+}
+</script>
+<style lang='scss'>
+.no-objects {
+  opacity: 0.5;
+}
+</style>
