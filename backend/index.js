80fe1204a69b71db40d1afbf1760d65bb4f05792
Erik Elze <info@erikelze-events.de>
2021-01-07 22:23:55 +0100

    edit

--- backend/index.js
+++ backend/index.js
@@ -2,28 +2,26 @@
 Dependencies
 */
 
-const express = require('express')
-const admin = require('firebase-admin');
-let inspect = require('util').inspect;
-let Busboy = require('busboy');
+  const express = require('express')
+  const admin = require('firebase-admin');
 
 /* 
 Config - Express
 */
 
-const app = express()
+  const app = express()
 
 /* 
 Config - Firebase
 */
 
-const serviceAccount = require('./serviceAccountKey.json');
+  const serviceAccount = require('./serviceAccountKey.json');
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
 
-const db = admin.firestore();
+  const db = admin.firestore();
 
 /* 
 Endpoint - Get Material DB
@@ -45,43 +43,13 @@ Endpoint - Get Material DB
 Endpoint - Add Material
 */
 
-app.post('/addObject', (request, response) => {
-  response.set('Access-Control-Allow-Origin', '*')
-
-  var busboy = new Busboy({ headers: request.headers });
-
-  let fields = {}
-
-/*   busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-    console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-    file.on('data', function(data) {
-      console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-    });
-    file.on('end', function() {
-      console.log('File [' + fieldname + '] Finished');
-    });
-  }); */
-
-  busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) {
-    console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-    fields[fieldname] = val
-  });
-
-  busboy.on('finish', function() {
-    db.collection('objects').doc(fields.id).set({
-      title: fields.title,
-      packed: fields.packed,
-      price: fields.price,
-    })
+  app.post('/addObject', (request, response) => {
+    response.set('Access-Control-Allow-Origin', '*')
     
-    response.send('Done parsing form!');
-  });
-
-  request.pipe(busboy);
-})
+  })
 
 /* 
 Listen
 */
 
-app.listen(process.env.PORT || 3000)
+  app.listen(process.env.PORT || 3000)
